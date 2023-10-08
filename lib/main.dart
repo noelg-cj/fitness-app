fbb75a77443c680f9668c1260df38f9a5970544b
Noel George C J <noelgeorge2004@gmail.com>
2023-10-08 13:33:57 +0530

    Added appBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Poppins'),
-      home: homePage(
+      home: const homePage(
 
       ),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class homePage extends StatelessWidget {
   const homePage({super.key});
@@ -6,12 +9,64 @@ class homePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Breakfast"
-        
+      appBar: appBar(),
+
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        "Breakfast",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
+        )
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+
+        },
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/backArrow.svg',
+            height: 20,
+            width: 20,
+            ),
         ),
       ),
 
+      actions: [
+        GestureDetector(
+          onTap: () {
+            
+          },
+          child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            height: 5,
+            width: 5,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
