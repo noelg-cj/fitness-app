f73f7ab08f4a814598821c7f0834f6d32b005497
Noel George C J <noelgeorge2004@gmail.com>
2023-10-08 11:00:38 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firstapp/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: homePage(
+
+      ),
+    );
+  }
+}
