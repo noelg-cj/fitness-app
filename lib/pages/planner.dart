aa629904e4e684b47c03777d1da7dbbbf3026032
Noel George C J <noelgeorge2004@gmail.com>
2023-10-12 20:41:40 +0530

    Added nutritions graph

--- lib/pages/planner.dart
+++ lib/pages/planner.dart
@@ -9,58 +9,108 @@ class planner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Meal Planner',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
+      appBar: _appBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Meal Nutritions',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600
+                  )
+                ),
+                Container(
+                  height: 30,
+                  width: 75,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: const [
+                        Color(0xff9DCEFF),
+                        Color(0xff92A3FD)
+                      ]
+                    ),
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Weekly',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
+          Center(
+            child: SvgPicture.asset('assets/icons/Graph.svg'),
+          )
+        ],
+      )
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        'Meal Planner',
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
         ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        leading: GestureDetector(
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          print("hi");
+        },
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/backArrow.svg',
+            height: 20,
+            width: 20,
+            ),
+        ),
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {
-            print("hi");
+            print("Action clicked");
           },
           child: Container(
             margin: EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 37,
             decoration: BoxDecoration(
               color: Color(0xffF7F8F8),
               borderRadius: BorderRadius.circular(10)
             ),
             child: SvgPicture.asset(
-              'assets/icons/backArrow.svg',
-              height: 20,
-              width: 20,
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
               ),
           ),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-              print("Action clicked");
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              alignment: Alignment.center,
-              width: 37,
-              decoration: BoxDecoration(
-                color: Color(0xffF7F8F8),
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: SvgPicture.asset(
-                'assets/icons/dots.svg',
-                height: 5,
-                width: 5,
-                ),
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
